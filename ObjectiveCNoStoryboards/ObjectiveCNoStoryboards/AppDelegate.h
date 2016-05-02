b1798dba36f69a6875d8e3113dac781f2533ba5f
Alan O'Connor <alanoconnor@Alans-MacBook-Pro.local>
2016-05-02 01:43:23 +0100

    created project

---
+++ ObjectiveCNoStoryboards/ObjectiveCNoStoryboards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCNoStoryboards
+//
+//  Created by Alan O'Connor on 02/05/2016.
+//  Copyright © 2016 Alan O'Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
