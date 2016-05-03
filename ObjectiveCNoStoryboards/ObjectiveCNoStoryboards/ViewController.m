c7085b269de845b5cebc9fcf7bdf273ae549b58c
O'Connor, Alan <Alan.O'Connor@paddypower.com>
2016-05-03 11:45:00 +0100

    quick tidy

--- ObjectiveCNoStoryboards/ObjectiveCNoStoryboards/ViewController.m
+++ ObjectiveCNoStoryboards/ObjectiveCNoStoryboards/ViewController.m
@@ -17,27 +17,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)didSelectTableViewButton:(UIButton *)sender {
 	TableViewController *tableViewController = [[TableViewController alloc] initWithNibName:@"TableViewController" bundle:[NSBundle mainBundle]];
 	[self.navigationController pushViewController:tableViewController animated:YES];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
